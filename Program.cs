cdf80d86beb0d819b5c3934b3ced7b13bf925076
Maciej Drozdowicz <maciek.d@me.com>
2021-06-09 11:43:30 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class Program
+{
+    private const string _clientId = "";
+    private const string _tenantId = "";
+
+    public static async Task Main(string[] args)
+    {
+        IPublicClientApplication app;
+
+        app = PublicClientApplicationBuilder
+            .Create(_clientId)
+            .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+            .WithRedirectUri("http://localhost")
+            .Build();
+
+        List<string> scopes = new List<string>
+        {
+            "user.read"
+        };
+
+        DeviceCodeProvider provider = new DeviceCodeProvider(app, scopes);
+
+        GraphServiceClient client = new GraphServiceClient(provider);
+
+        User myProfile = await client.Me
+            .Request()
+            .GetAsync();
+
+        Console.WriteLine($"Name:\t{myProfile.DisplayName}");
+        Console.WriteLine($"AAD Id:\t{myProfile.Id}");
+    }
+
+
+}
